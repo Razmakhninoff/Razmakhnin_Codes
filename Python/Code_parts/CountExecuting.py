1e07034e4ada95eab8c9d1e390721875605b4352
Razmakhnin <110770382+Razmakhninoff@users.noreply.github.com>
2022-09-13 18:12:01 +0300

    Add files via upload

---
+++ Python/Code_parts/CountExecuting.py
@@ -0,0 +1,7 @@
+
+# use only for performance measurement.
+start = time.perf_counter()
+end = time.perf_counter()
+total_time = end - start
+print(f"Your program has executed for {total_time} seconds.")
+# Your program has executed for 0.036370800000000036 seconds.
